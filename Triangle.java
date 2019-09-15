b6f3cadebd968d1c6f32f2056971aaf0d7307454
dileeppdk <54081492+dileeppdk@users.noreply.github.com>
2019-09-14 23:35:58 -0500

    starstriangle

---
+++ Triangle.java
@@ -0,0 +1,26 @@
+package com.triangle;
+
+import java.util.Scanner;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number");
+		int n = sc.nextInt();
+		for(int i=0;i<n ;i++)
+		{
+		for (int j=0;j<n-i-1;j++)
+		{
+		System.out.print(" ");
+		}
+		for(int k=0;k<=i; k++)
+		{
+		System.out.print("* ");
+		}
+		System.out.println( );
+		}
+	}
+
+}
