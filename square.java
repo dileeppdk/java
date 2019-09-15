43d2410c842a38e562eb35a925e579d229d661e1
dileeppdk <54081492+dileeppdk@users.noreply.github.com>
2019-09-14 22:44:29 -0500

    square

---
+++ square.java
@@ -0,0 +1,16 @@
+package com.evenodd;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number: ");
+		int n=sc.nextInt();
+         n = n * n;
+			System.out.println(n);
+		
+		
+	}
+
+}
